4280ef7f29fb84c94565e1d9d2b31c247b9135bd
AlexanderDuong <54642720+AlexanderDuong@users.noreply.github.com>
2019-11-05 19:29:52 -0500

    WEek 8 Carnival Game Attempt

---
+++ Week 8 GD205/Assets/Scripts/Playermove.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playermove : MonoBehaviour
+{
+    public float Speed;
+    public float force;
+    public float toss;
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+
+        if (Input.GetMouseButton(0))
+        {
+
+            Ray laser = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit = new RaycastHit();
+
+            if (Physics.Raycast(laser, out hit, 10000f))
+            {
+                Debug.Log("you hit" + hit.transform.gameObject.name + " with the ray");
+
+                if (hit.rigidbody)
+                {
+                    hit.rigidbody.AddForce(0f, force, 0f);
+                }
+            }
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+
+            Ray laser = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit = new RaycastHit();
+
+            if (Physics.Raycast(laser, out hit, 10000f))
+            {
+                Debug.Log("you hit" + hit.transform.gameObject.name + " with the ray");
+
+                if (hit.rigidbody)
+                {
+                    hit.rigidbody.AddForce(0f, 0f, toss);
+                }
+            }
+        }
+    }
+
+    void PlayerMovement()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        //float ver = Input.GetAxis("Vertical");
+        Vector3 playerMovement = new Vector3(hor, 0f, 0f) * Speed * Time.deltaTime;
+        transform.Translate(playerMovement, Space.Self);
+
+    }
+
+}
---
+++ Week 8 GD205/Assets/Scripts/Reset.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Reset : MonoBehaviour
+{
+    public Transform playerpiece;
+
+    public void resetball()
+    {
+        playerpiece.position = new Vector3(-1f, 1f, -2f);
+    }
+}
